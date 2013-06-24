d89fd37e4b88e43b868c04024cf5af231ac4674c
Josh Burton <athor3d@gmail.com>
2013-06-24 12:00:31 +1200

    remove not needed files

--- gen/uk/co/sevendigital/android/partner/instantpurchase/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package uk.co.sevendigital.android.partner.instantpurchase;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
