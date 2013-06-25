50dc2713cefb20adb5a5a055f82b71d1c5cd61ba
Josh Burton <athor3d@gmail.com>
2013-06-25 16:20:14 +1200

    worked on mockup

---
+++ sample/src/uk/co/sevendigital/android/partner/instantpurchase/sample/SDICustomTrackDialog.java
@@ -0,0 +1,106 @@
+package uk.co.sevendigital.android.partner.instantpurchase.sample;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.actionbarsherlock.app.SherlockDialogFragment;
+
+public class SDICustomTrackDialog extends SherlockDialogFragment {
+	public static SDICustomTrackDialog newInstance() {
+		SDICustomTrackDialog f = new SDICustomTrackDialog();
+		return f;
+	}
+
+	public SDICustomTrackDialog() {}
+
+	public interface TrackDialogFragmentListener {
+		public void onBuyAlbum(SDICustomTrackDialog customDialogFragment);
+
+		public void onBuyTrack(SDICustomTrackDialog customDialogFragment);
+	}
+
+	static TrackDialogFragmentListener mListener;
+	private EditText mReleaseEditText;
+	private EditText mTrackEditText;
+	private EditText mPartnerEditText;
+	private EditText mCountryEditText;
+
+	@Override public Dialog onCreateDialog(Bundle savedInstanceState) {
+		mListener = (TrackDialogFragmentListener) getActivity();
+		LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+		View view = inflater.inflate(R.layout.custom_track_dialog, null);
+
+		mReleaseEditText = (EditText) view.findViewById(R.id.track_release_id_edittext);
+		mTrackEditText = (EditText) view.findViewById(R.id.track_id_edittext);
+		mPartnerEditText = (EditText) view.findViewById(R.id.affiliate_id_edittext);
+		mCountryEditText = (EditText) view.findViewById(R.id.country_code_edittext);
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(getSherlockActivity());
+		builder.setCancelable(false);
+		builder.setView(view);
+		builder.setInverseBackgroundForced(true);
+		builder.setTitle("Enter purchase details");
+		builder.setPositiveButton("Buy Track", null);
+		builder.setNeutralButton("Buy Album", new DialogInterface.OnClickListener() {
+
+			@Override public void onClick(DialogInterface dialog, int which) {
+				mListener.onBuyAlbum(SDICustomTrackDialog.this);
+
+			}
+		});
+		final AlertDialog adialog = builder.create();
+		adialog.setOnDismissListener(this);
+		adialog.setOnCancelListener(this);
+
+		return builder.create();
+	}
+
+	public void onOkButtonClick() {
+		final String release = mReleaseEditText.getText().toString();
+		final String track = mTrackEditText.getText().toString();
+
+		if (TextUtils.isEmpty(release) || TextUtils.isEmpty(track)) {
+			mReleaseEditText.setError("Enter something");
+			return;
+		} else {
+			mListener.onBuyTrack(SDICustomTrackDialog.this);
+		}
+	}
+
+	public String getRelease() {
+		return mReleaseEditText.getText().toString();
+	}
+
+	public String getTrack() {
+		return mTrackEditText.getText().toString();
+	}
+
+	public String getPartner() {
+		return mPartnerEditText.getText().toString();
+	}
+
+	public String getCountryCode() {
+		return mCountryEditText.getText().toString();
+	}
+
+	@Override public void onResume() {
+		super.onResume();
+		AlertDialog alertDialog = (AlertDialog) getDialog();
+		Button okButton = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE);
+		okButton.setOnClickListener(new View.OnClickListener() {
+			@Override public void onClick(View v) {
+				onOkButtonClick();
+
+			}
+		});
+	}
+}
---
+++ sample/src/uk/co/sevendigital/android/partner/instantpurchase/sample/SDIPurchaseExampleActivity2.java
@@ -0,0 +1,128 @@
+package uk.co.sevendigital.android.partner.instantpurchase.sample;
+
+import uk.co.sevendigital.android.partner.instantpurchase.SDIPurchaseFragmentActivity;
+import uk.co.sevendigital.android.partner.instantpurchase.sample.SDICustomTrackDialog.TrackDialogFragmentListener;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
+import com.crashlytics.android.Crashlytics;
+
+/**
+ * The purpose of this activity is to test launching and interfacing with the 7digital application.
+ * 
+ */
+public class SDIPurchaseExampleActivity2 extends SherlockFragmentActivity implements TrackDialogFragmentListener {
+
+	private ImageButton mFavouriteButton;
+	private ImageButton mShareButton;
+	private Button mBuyButton;
+	private ImageButton mPauseButton;
+	private TextView mTitleTextview;
+	private TextView mArtistTextview;
+	private ImageButton mNextButton;
+	private SeekBar mSeekBar;
+
+	/** Called when the activity is first created. */
+	@Override public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		if (!BuildConfig.DEBUG) Crashlytics.start(this);
+		setContentView(R.layout.main_activity2);
+
+		mFavouriteButton = (ImageButton) findViewById(R.id.favourite_button);
+		mShareButton = (ImageButton) findViewById(R.id.share_button);
+		mBuyButton = (Button) findViewById(R.id.buy_button);
+		mPauseButton = (ImageButton) findViewById(R.id.pause_button);
+		mTitleTextview = (TextView) findViewById(R.id.title_textview);
+		mArtistTextview = (TextView) findViewById(R.id.artist_textview);
+		mNextButton = (ImageButton) findViewById(R.id.next_button);
+		mSeekBar = (SeekBar) findViewById(R.id.seekBar);
+
+		mBuyButton.setOnClickListener(new OnClickListener() {
+
+			@Override public void onClick(View v) {
+				Long releaseId = (long) 2235853;
+				Long trackId = (long) 24058764;
+				SDIPurchaseFragmentActivity.startActivity(SDIPurchaseExampleActivity2.this, releaseId, trackId, null, "US");
+			}
+		});
+
+		mSeekBar.setProgress(70);
+	}
+
+	@Override public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getSupportMenuInflater();
+		inflater.inflate(R.menu.home_menu, menu);
+		return true;
+	}
+
+	@Override public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.buy_album_item: {
+			SDIPurchaseFragmentActivity.startActivity(this, (long) 1234, null, null, null);
+			return true;
+		}
+		case R.id.buy_track_item: {
+			SDIPurchaseFragmentActivity.startActivity(this, null, (long) 12345, null, null);
+			return true;
+		}
+		case R.id.buy_specific_track_item: {
+			SDIPurchaseFragmentActivity.startActivity(this, (long) 1302, (long) 12345, null, null);
+			return true;
+		}
+		case R.id.buy_us_track_item: {
+			SDIPurchaseFragmentActivity.startActivity(this, null, (long) 23243634, (long) 712, "US");
+			return true;
+		}
+		case R.id.buy_track_partner_item: {
+			SDIPurchaseFragmentActivity.startActivity(this, null, (long) 12345, (long) 712, null);
+			return true;
+		}
+		case R.id.free_track_item: {
+			SDIPurchaseFragmentActivity.startActivity(this, null, (long) 8515447, null, null);
+			return true;
+		}
+		case R.id.custom_track_item: {
+			SDICustomTrackDialog dialog = SDICustomTrackDialog.newInstance();
+			dialog.show(getSupportFragmentManager(), "CustomTrackDialog");
+			return true;
+		}
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override public void onBuyAlbum(SDICustomTrackDialog customDialogFragment) {
+		Long album = getLongFromString(customDialogFragment.getRelease());
+		Long partnerId = getLongFromString(customDialogFragment.getPartner());
+		String countryCode = customDialogFragment.getCountryCode();
+		SDIPurchaseFragmentActivity.startActivity(this, album, null, partnerId, countryCode);
+	}
+
+	@Override public void onBuyTrack(SDICustomTrackDialog customDialogFragment) {
+		Long album = getLongFromString(customDialogFragment.getRelease());
+		Long track = getLongFromString(customDialogFragment.getTrack());
+		Long partnerId = getLongFromString(customDialogFragment.getPartner());
+		String countryCode = customDialogFragment.getCountryCode();
+		SDIPurchaseFragmentActivity.startActivity(this, album, track, partnerId, countryCode);
+	}
+
+	private Long getLongFromString(String edittext) {
+		if (TextUtils.isEmpty(edittext)) return null;
+		try {
+			return Long.parseLong(edittext);
+		} catch (NumberFormatException e) {
+			return null;
+		}
+	}
+
+}
